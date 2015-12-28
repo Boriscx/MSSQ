ade2dbee068b0a26bc7c4ddfd2a02866f71a1db4
Boriscx <564732485@qq.com>
2015-12-28 13:31:34 +0800

    bcx

---
+++ app/src/main/java/com/example/bcx/dao/basedao.java
@@ -0,0 +1,10 @@
+package com.example.bcx.dao;
+
+/**
+ * Created by bcx on 2015/12/28.
+ */
+public interface basedao<T> {
+    public void add(T t);
+    public void delete(T t);
+    public void locainfo(T t);
+}
---
+++ app/src/main/java/com/example/bcx/dao/basedaoimpl.java
@@ -0,0 +1,26 @@
+package com.example.bcx.dao;
+
+
+
+/**
+ * Created by bcx on 2015/12/28.
+ */
+public class basedaoimpl implements basedao {
+
+    @Override
+    public void add(Object o) {
+        int abdd;
+        abdd=2;
+        System.out.println(abdd);
+    }
+
+    @Override
+    public void delete(Object o) {
+
+    }
+
+    @Override
+    public void locainfo(Object o) {
+
+    }
+}
