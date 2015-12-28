3d2d84cb47f8059a760043425bfffceec0c754fa
Boriscx <564732485@qq.com>
2015-12-28 14:53:09 +0800

    bcx

--- app/src/main/java/com/example/bcx/dao/basedaoimpl.java
+++ app/src/main/java/com/example/bcx/dao/basedaoimpl.java
@@ -9,10 +9,7 @@ public class basedaoimpl implements basedao {
 
     @Override
     public void add(Object o) {
-        int abdd;
-        abdd=211;
-        System.out.println();
-        System.out.println(abdd);
+        
     }
 
     @Override
